3b8957d5124db5d82152bb5b9808af4137ffce88
karita <shigekikarita@gmail.com>
2017-12-31 15:29:39 +0900

    add bindings.c

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,4 +1,5 @@
 open Ocamlbuild_plugin;;
 
+(* TODO: use C++ https://www.versti.eu/TranslateProxy/https/github.com/ygrek/ocaml-hypertable/blob/master/myocamlbuild.ml *)
 flag ["link"; "ocaml"; "gcc"] (S[A"-cc"; A"gcc"]);;
 dep ["link"; "ocaml"; "use_bindings"] ["bindings.o"];;
